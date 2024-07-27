f73452033b46775de05a4b1789ff79c6ea959191
milenacamargo20 <milenacamarggo20@gmail.com>
2024-07-26 22:04:57 -0300

    fix: delete logout route

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const authController = require ('../controllers/authController');
 
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
 
 module.exports = router;
